656d22f3acce047e1bbd70223826c21bf5e833e7
krzysztof-grzybek <krzysztof.grzybek89@gmail.com>
2018-08-22 19:44:53 +0200

    travis fix

--- spec/lintRunner.ts
+++ spec/lintRunner.ts
@@ -5,8 +5,8 @@ export const helper = () => {
     const program = Linter.createProgram('spec/tsconfig.json');
     const linter = new Linter({fix: false}, program);
 
-    const files = Linter.getFileNames(program)
-    files.forEach(file => {
+    const files = Linter.getFileNames(program);
+    files.forEach((file) => {
         const fileContents = program.getSourceFile(file).getFullText();
         const configuration = Configuration.findConfiguration(configurationFilename, file).results;
         linter.lint(file, fileContents, configuration);
